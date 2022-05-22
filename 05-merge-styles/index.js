bac4e2c18ca0b0e7361b1d35ce70acb9f48cac8a
marat1000 <marat76593@gmail.com>
2022-05-23 02:33:14 +0300

    feat: implement task 5

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const path = require('path');
+
+const fsPromises = fs.promises;
+const folderInit = path.join(__dirname, 'styles');
+const bundle = path.join(__dirname, 'project-dist', 'bundle.css');
+
+fsPromises.rm(bundle, {recursive: true, force: true}).then(function () {
+  copy();
+}).catch(function () {
+  copy();
+});
+
+function copy() {
+  fs.readdir(folderInit, {withFileTypes: true}, function (err, items) {
+    fs.createWriteStream(bundle);
+    for (const item of items) {
+      if (item.isFile() && path.extname(item.name) === '.css') {
+        let css = '';
+        const pathToCSS = path.join(folderInit, path.basename(item.name));
+        const stream = fs.createReadStream(pathToCSS, 'utf-8');
+        stream.on('data', chunk => css += chunk);
+        stream.on('end', () => {
+          css = `${css}
+
+`;
+          fs.appendFile(
+            bundle,
+            css,
+            (err) => {
+              if (err) throw err;
+            }
+          );
+        });
+      }
+    }
+  });
+}
