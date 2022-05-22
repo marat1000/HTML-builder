2e4752f43f60f478cf910d2a8d20be223c4e8212
marat1000 <marat76593@gmail.com>
2022-05-23 01:02:37 +0300

    feat: implement task 4

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -7,7 +7,7 @@ fs.readdir(folder, {withFileTypes: true}, function (err, items) {
     if (item.isFile()) {
       const ext = path.extname(item.name);
       const extShort = ext.slice(1);
-      const pathToFile = folder + '\\' + item.name;
+      const pathToFile = path.join(folder, item.name);
       const name = path.basename(item.name, ext);
       fs.stat(pathToFile, function(err, stats) {
         console.log(`${name} - ${extShort} - ${stats.size}`);
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,25 @@
+const fs = require('fs');
+const path = require('path');
 
+const fsPromises = fs.promises;
+const folderInit = path.join(__dirname, 'files');
+const folder = `${folderInit}-copy`;
+
+fsPromises.rm(folder, {recursive: true, force: true}).then(function () {
+  copy();
+}).catch(function () {
+  copy();
+});
+
+function copy() {
+  fsPromises.mkdir(folder, {recursive: true}).then(function () {}).catch(function () {});
+  fs.readdir(folderInit, {withFileTypes: true}, function (err, items) {
+    for (const item of items) {
+      if (item.isFile()) {
+        const pathToFile = path.join(folderInit, item.name);
+        const pathToFile2 = path.join(folder, item.name);
+        fsPromises.copyFile(pathToFile, pathToFile2).then(function () {}).catch(function () {});
+      }
+    }
+  });
+}
